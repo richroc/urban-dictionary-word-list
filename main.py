142aa632af0812b14c876a7716207cfe5b03cc95
Taffy <richroc@users.noreply.github.com>
2024-10-07 15:45:13 -0700

    Update main.py
    Updated "API" URL.
    Added User-Agent header to requests.
    Updated some variable names
    Added some additional error handling.

--- main.py
+++ main.py
@@ -1,126 +1,111 @@
-import argparse
-import itertools
 import string
-import time
-import urllib.request
-
 from bs4 import BeautifulSoup
+import urllib.request
+import time
+import os
+import argparse
+import re
 
 API = "https://www.urbandictionary.com/browse.php?character={0}"
 
-MAX_ATTEMPTS = 10
+MAX_ATTEMPTS = 4
 DELAY = 10
 
-NUMBER_SIGN = "*"
+NUMBER_SIGN = "#"
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+                  '(KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+}
 
-# https://stackoverflow.com/a/554580/306149
-class NoRedirection(urllib.request.HTTPErrorProcessor):
-    def http_response(self, request, response):
-        return response
+# Use the default opener with cookie support
+opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor())
+urllib.request.install_opener(opener)
 
-    https_response = http_response
-
-
-def extract_page_entries(html):
+def extract_page_entries(letter, html):
     soup = BeautifulSoup(html, "html.parser")
-    # find word list element, this might change in the future
-    ul = soup.find_all("ul", class_="mt-3 columns-2 md:columns-3")[0]
-    for li in ul.find_all("li"):
-        a = li.find("a").string
-        if a:
-            yield a
+    columnist = soup.find(id="columnist")
+    if not columnist:
+        return
+    ul_list = columnist.find('ul')
+    if not ul_list:
+        return
+    for li in ul_list.find_all('li'):
+        a_tag = li.find('a')
+        if a_tag and a_tag.string:
+            a = a_tag.string
+            if letter == NUMBER_SIGN:
+                if not re.match('[a-zA-Z]', a):
+                    yield a
+            else:
+                if a.lower().startswith(letter.lower()):
+                    yield a
 
-
-def get_next(html):
+def get_next(letter, html):
     soup = BeautifulSoup(html, "html.parser")
-    next_link = soup.find("a", {"rel": "next"})
+    next_link = soup.find('a', {"rel": "next"})
     if next_link:
-        href = next_link["href"]
-        return "https://www.urbandictionary.com" + href
+        href = next_link['href']
+        return 'https://www.urbandictionary.com' + href
     return None
 
-
 def extract_letter_entries(letter):
-    url = API.format(letter)
+    if letter == NUMBER_SIGN:
+        start = '*'  # URL-encoded '#'
+    else:
+        start = letter + 'a'
+    url = API.format(start)
     attempt = 0
     while url:
         print(url)
-        response = urllib.request.urlopen(url)
-        code = response.getcode()
-        if code == 200:
-            content = response.read()
-            yield list(extract_page_entries(content))
-            url = get_next(content)
-            attempt = 0
-        else:
-            print(f"Trying again, expected response code: 200, got {code}")
-            attempt += 1
-            if attempt > MAX_ATTEMPTS:
-                break
-            time.sleep(DELAY * attempt)
+        try:
+            req = urllib.request.Request(url, headers=headers)
+            response = urllib.request.urlopen(req)
+            code = response.getcode()
+            if code == 200:
+                content = response.read().decode('utf-8')
+                yield list(extract_page_entries(letter, content))
+                url = get_next(letter, content)
+                attempt = 0
+            else:
+                print(f"Trying again, expected response code: 200, got {code}")
+                attempt += 1
+                if attempt > MAX_ATTEMPTS:
+                    break
+                time.sleep(DELAY * attempt)
+        except Exception as e:
+            print(f"Error fetching {url}: {e}")
+            break
+        time.sleep(DELAY)
 
-
-opener = urllib.request.build_opener(
-    NoRedirection, urllib.request.HTTPCookieProcessor()
-)
-urllib.request.install_opener(opener)
-
-
-letters = list(string.ascii_uppercase) + ["#"]
-
-
-def download_letter_entries(letter, file, remove_dead):
+def download_letter_entries(letter, file):
     file = file.format(letter)
-    entries = itertools.chain.from_iterable(list(extract_letter_entries(letter)))
+    directory = os.path.dirname(file)
+    if directory:
+        os.makedirs(directory, exist_ok=True)
+    for entry_set in extract_letter_entries(letter):
+        with open(file, 'a', encoding='utf-8') as f:
+            data = ('\n'.join(entry_set))
+            f.write(data + '\n')
 
-    if remove_dead:
-        all_data = entries
-    else:
-        with open(file, "r", encoding="utf-8") as f:
-            old_data = [line.strip() for line in f.readlines()]
-        all_data = sorted(set(old_data).union(set(entries)), key=str.casefold)
-
-    with open(file, "w", encoding="utf-8") as f:
-        f.write("\n".join(all_data) + "\n")
-
-
-def download_entries(letters, file, remove_dead):
+def download_entries(letters, file):
     for letter in letters:
         print(f"======={letter}=======")
-        download_letter_entries(letter, file, remove_dead)
+        download_letter_entries(letter, file)
 
+parser = argparse.ArgumentParser(description='Process some integers.')
 
-parser = argparse.ArgumentParser(description="Download urban dictionary words.")
+parser.add_argument('--ifile', dest='ifile',
+                    help='Input file name. Contains a list of letters separated by a newline', default="input.list")
 
-parser.add_argument(
-    "letters", metavar="L", type=str, nargs="*", help="Letters to download."
-)
-
-parser.add_argument(
-    "--ifile",
-    dest="ifile",
-    help="input file name. Contains a list of letters separated by a newline",
-    default="input.list",
-)
-
-parser.add_argument(
-    "--out",
-    dest="out",
-    help="output file name. May be a format string",
-    default="data/{0}.data",
-)
-
-parser.add_argument(
-    "--remove-dead", action="store_true", help="Removes entries that no longer exist."
-)
+parser.add_argument('--out', dest='out',
+                    help='Output file name. May be a format string', default="data/{0}.data")
 
 args = parser.parse_args()
 
-letters = [letter.upper() for letter in args.letters]
-if not letters:
-    with open(args.ifile, "r") as ifile:
-        for row in ifile:
-            letters.append(row.strip())
+letters = []
+with open(args.ifile, 'r') as ifile:
+    for row in ifile:
+        letters.append(row.strip())
 
-download_entries(letters, args.out, args.remove_dead)
+download_entries(letters, args.out)
